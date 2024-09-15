ca339fc9097d36f5b29419250c17075d3df6c5e4
suhani tamrkar <suhanitamrkar2004@gmail.com>
2024-09-15 21:00:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// let courses = ["HTML","CSS","JAVASCRIPT","REACT","NODE.JS","NEXTJS" ]
+// console.log(courses,"courses Name");
+
+// let lastItem = courses[courses.length -3]
+// console.log("first Item" ,lastItem);
+
+// let age = 24
+// console.log(age,"this is my brother's age");
+// console.log("suhani tamrkar");
+
+// let price = 100.3
+// console.log(price);
+
+//objects
+// const student = {
+//     name: "suhani tamrkar",
+//     age: 20,
+//     cgpa: 9.3,
+//     isPass:true
+// }
+// student["age"]=student["age"] +2 // to change the value
+// student['name'] ='tanu tamrkar'
+// console.log(student['age']);
+// console.log(student.name);
+
+const product= {
+    item:'pen',
+    name:'Parkar Jotter',
+    rating: 3,
+    deal: true,
+    price:200,
+    offer: true
+
+}
+const profile ={
+    name:'sharadha',
+    posts:195,
+    followers:89433,
+    description:"para",
+
+
+}
+console.log(product);
+console.log(profile.description);
+
+
+
